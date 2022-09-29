4971526c8e45ebc77d877627ba2d3c5d2d2f2e26
Jdbrown329 <112967429+Jdbrown329@users.noreply.github.com>
2022-09-29 16:29:52 -0500

    Add files via upload

---
+++ Loops/Loops/Program.cs
@@ -0,0 +1,25 @@
+﻿for (int i = 0; i < 5; i++)
+{ 
+    Console.WriteLine($"{i}"); 
+}
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+for (int i =0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
